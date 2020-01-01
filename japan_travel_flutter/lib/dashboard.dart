94970a89935a0a9a30fe88046d15b349a6277321
Данил Ченцов <danil090598@gmail.com>
2020-01-01 17:01:00 +0300

    card

--- japan_travel_flutter/lib/dashboard.dart
+++ japan_travel_flutter/lib/dashboard.dart
@@ -58,8 +58,86 @@ class _DashBoardPageState extends State<DashBoardPage> {
         SizedBox(height: 10),
         Container(
           height: MediaQuery.of(context).size.height - 156,
+          child: ListView(
+            children: <Widget>[
+              buildListItem('images/japan.jpg', 'Japan',
+                  'Explore the land of the rising sun')
+            ],
+          ),
         )
       ],
+    );
+  }
+
+  buildListItem(String imgPath, String country, String description) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            height: 300,
+          ),
+          Container(
+            height: 300,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                image: DecorationImage(
+                    image: AssetImage(imgPath),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.4), BlendMode.darken))),
+          ),
+          Container(
+            height: 300,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    country,
+                    style: GoogleFonts.montserrat(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        textStyle: TextStyle(color: Colors.white)),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    description,
+                    style: GoogleFonts.montserrat(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        textStyle: TextStyle(color: Colors.white)),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      child: Center(
+                        child: Text(
+                          'Explore Now',
+                          style: GoogleFonts.montserrat(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              textStyle: TextStyle(color: Color(0xFFFD4F99))),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(7),
+                          color: Colors.white),
+                      width: 125,
+                      height: 50,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
